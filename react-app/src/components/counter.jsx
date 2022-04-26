28aeb336c85fbfac850c72872890f759f584f0d1
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-04-26 21:00:33 +0530

    rendered counter with some button and span css

---
+++ react-app/src/components/counter.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+      count: 0
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <span style={{fontSize:11}} className="badge bg-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+  formatCount() {
+      const {count} = this.state;
+      return count === 0 ? 'Zero' : count;
+  }
+}
+
+export default Counter;
