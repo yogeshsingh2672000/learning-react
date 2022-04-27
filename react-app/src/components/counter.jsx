528c47c586de1f606c3a32759169384237a467dc
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-04-27 13:03:00 +0530

    dynamic css classes

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -7,11 +7,17 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span style={{fontSize:11}} className="badge bg-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    let classes = 'badge m-2 bg-';
+    classes += (this.state.count) === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+
   formatCount() {
       const {count} = this.state;
       return count === 0 ? 'Zero' : count;
